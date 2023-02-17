d3e55d8292f285618232e2fc9d8ffc1c25b4ccc4
Trent Hauck <trent@trenthauck.com>
2023-02-16 18:49:04 -0800

    build: rename

--- src/include/wtt01_extension.hpp
+++ src/include/wtt01_extension.hpp
@@ -4,7 +4,7 @@
 
 namespace duckdb {
 
-class QuackExtension : public Extension {
+class Wtt01Extension : public Extension {
 public:
 	void Load(DuckDB &db) override;
 	std::string Name() override;
--- src/wtt01_extension.cpp
+++ src/wtt01_extension.cpp
@@ -1,6 +1,6 @@
 #define DUCKDB_EXTENSION_MAIN
 
-#include "quack_extension.hpp"
+#include "wtt01_extension.hpp"
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
@@ -11,12 +11,12 @@
 
 namespace duckdb {
 
-inline void QuackScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+inline void Wtt01ScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
     auto &name_vector = args.data[0];
     UnaryExecutor::Execute<string_t, string_t>(
 	    name_vector, result, args.size(),
 	    [&](string_t name) { 
-			return StringVector::AddString(result, "Quack "+name.GetString()+" 🐥");;
+			return StringVector::AddString(result, "Wtt01 "+name.GetString()+" 🐥");;
         });
 }
 
@@ -26,29 +26,29 @@ static void LoadInternal(DatabaseInstance &instance) {
 
     auto &catalog = Catalog::GetSystemCatalog(*con.context);
 
-    CreateScalarFunctionInfo quack_fun_info(
-            ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun));
-    quack_fun_info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
-    catalog.CreateFunction(*con.context, &quack_fun_info);
+    CreateScalarFunctionInfo wtt01_fun_info(
+            ScalarFunction("wtt01", {LogicalType::VARCHAR}, LogicalType::VARCHAR, Wtt01ScalarFun));
+    wtt01_fun_info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+    catalog.CreateFunction(*con.context, &wtt01_fun_info);
     con.Commit();
 }
 
-void QuackExtension::Load(DuckDB &db) {
+void Wtt01Extension::Load(DuckDB &db) {
 	LoadInternal(*db.instance);
 }
-std::string QuackExtension::Name() {
-	return "quack";
+std::string Wtt01Extension::Name() {
+	return "wtt01";
 }
 
 } // namespace duckdb
 
 extern "C" {
 
-DUCKDB_EXTENSION_API void quack_init(duckdb::DatabaseInstance &db) {
+DUCKDB_EXTENSION_API void wtt01_init(duckdb::DatabaseInstance &db) {
 	LoadInternal(db);
 }
 
-DUCKDB_EXTENSION_API const char *quack_version() {
+DUCKDB_EXTENSION_API const char *wtt01_version() {
 	return duckdb::DuckDB::LibraryVersion();
 }
 }
