640d436895e5c04f82291e0b7450eae7967157d7
Trent Hauck <trent@trenthauck.com>
2023-02-18 21:00:08 -0800

    build: try to deploy to dev

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,8 @@ FROM ubuntu:20.04 AS builder
 
 ARG DEBIAN_FRONTEND=noninteractive
 
-ARG CHECK_LICENSE
-ENV CHECK_LICENSE=${CHECK_LICENSE}
+RUN apt-get -y update && apt-get -y upgrade && apt-get -y install python3 python3-pip build-essential python-dev unzip
 
-ARG WTT_01_LICENSE_SERVER_URL
-ENV WTT_01_LICENSE_SERVER_URL=${WTT_01_LICENSE_SERVER_URL}
-
-RUN apt-get -y update && apt-get -y upgrade && apt-get -y install python3 python3-pip build-essential python-dev
+RUN pip3 install duckdb==0.7.0
 
 ADD ./wtt01.duckdb_extension /tmp/wtt01.duckdb_extension
---
+++ bin/upload-artifacts.py
@@ -0,0 +1,89 @@
+"""Helper script to upload artifacts"""
+import os
+import json
+import uuid
+import zipfile
+import platform
+from pathlib import Path
+
+import boto3
+
+client = boto3.client("s3")
+lambda_client = boto3.client("lambda")
+
+if __name__ == "__main__":
+    environment = os.environ["ENVIRONMENT"]
+    bucket = os.environ["ARTIFACT_BUCKET"]
+    handler = os.environ["ARTIFACT_HANDLER"]
+
+    print(f"Uploading artifacts to {bucket} with handler {handler}.")
+    print(f"Working on environment: {environment}.")
+
+    name = "wtt01"
+
+    platform_uname = platform.uname()
+    operating_system = platform_uname.system
+    architecture = platform_uname.machine
+
+    version = "0.1.5.dev3"
+
+    filename = f"{name}-{version}-{operating_system}-{architecture}.zip"
+    full_s3_path = f"s3://{bucket}/extension/{name}/{filename}"
+
+    local_file = Path(f"{name}.duckdb_extension.zip")
+
+    if operating_system.lower() == "windows":
+        raise RuntimeError("Windows is not supported yet.")
+        # if cmake_build_type == "Release":
+        #     build_target = (
+        #         Path("build") / "extension" / name / f"{name}.duckdb_extension"
+        #     )
+        # else:
+        #     build_target = (
+        #         Path("build")
+        #         / "release"
+        #         / "extension"
+        #         / "wtt01"
+        #         / f"{name}.duckdb_extension"
+        #     )
+    else:
+        build_target = Path("build") / "release" / "extension" / f"{name}.duckdb_extension"
+
+    with zipfile.ZipFile(local_file, "w") as zip_file:
+        print(f"Adding {build_target} to {local_file}.")
+        zip_file.write(str(build_target), arcname=f"{name}.duckdb_extension")
+
+    # Copy local_file to s3 location molcaat_full_s3_path.
+
+    upload_key = f"extension/{name}/{filename}"
+    client.upload_file(str(local_file), bucket, upload_key)
+
+    # Change the ACL of the file to public-read.
+    client.put_object_acl(
+        ACL="public-read",
+        Bucket=bucket,
+        Key=upload_key,
+    )
+
+    body = json.dumps(
+        {
+            "http": {"method": "POST"},
+            "body": {
+                "id": uuid.uuid4().hex,
+                "s3_key": full_s3_path,
+                "os": operating_system,
+                "arch": architecture,
+                "tag": version,
+                "tool": name,
+                "environment": environment,
+            },
+        }
+    )
+    print(body)
+
+    # Invoke the lambda function ARTIFACT_HANDLER with the body.
+    response = lambda_client.invoke(
+        FunctionName=handler,
+        Payload=bytes(body, "utf-8"),
+    )
+    print(response)
