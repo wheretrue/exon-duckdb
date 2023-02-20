6e53bf2e51452273bec9b3799537426cdb798a80
Trent Hauck <trent@trenthauck.com>
2023-02-19 20:08:58 -0800

    bump: version 0.1.7 → 0.1.8

--- src/wtt01_functions.cpp
+++ src/wtt01_functions.cpp
@@ -8,7 +8,7 @@
 namespace wtt01
 {
 
-    const std::string WTT_01_VERSION = "0.1.7";
+    const std::string WTT_01_VERSION = "0.1.8";
 
     duckdb::CreateScalarFunctionInfo Wtt01Functions::GetWtt01VersionFunction()
     {
