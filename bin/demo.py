e2fa0613c6df88f25bf7f62dd19951a7869acdb9
Trent Hauck <trent@trenthauck.com>
2023-02-19 19:09:54 -0800

    build: dev and prd docker

--- bin/demo.py
+++ bin/demo.py
@@ -6,10 +6,12 @@ data_dir = pathlib.Path(os.environ["DATA_DIR"])
 
 con = duckdb.connect(config={'allow_unsigned_extensions': True})
 
-con.load_extension('/tmp/wtt01.duckdb_extension')
+con.load_extension('/wtt/wtt01.duckdb_extension')
 
 vals = con.execute("SELECT gc_content('ATCG')").fetchall()
 print(vals)
 
-vals = con.execute(f"SELECT * FROM read_fasta('{data_dir / 'test.fasta'}')").fetchall()
+query = f"SELECT * FROM read_fasta('{data_dir / 'test.fasta'}')"
+print(query)
+vals = con.execute(query).fetchall()
 print(vals)
