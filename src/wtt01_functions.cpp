60b1d463a7aa69bdf283115e51421d11ae94c0dc
Trent Hauck <trent@trenthauck.com>
2023-02-21 11:31:27 -0800

    bump: version 0.1.14 → 0.1.15

--- src/wtt01_functions.cpp
+++ src/wtt01_functions.cpp
@@ -8,7 +8,7 @@
 namespace wtt01
 {
 
-    const std::string WTT_01_VERSION = "0.1.14";
+    const std::string WTT_01_VERSION = "0.1.15";
 
     duckdb::CreateScalarFunctionInfo Wtt01Functions::GetWtt01VersionFunction()
     {
--- wtt01_python/wtt01/__about__.py
+++ wtt01_python/wtt01/__about__.py
@@ -1,5 +1,5 @@
 """About wtt01."""
 
-__version__ = "0.1.14"
+__version__ = "0.1.15"
 
 EXTENSION_NAME = "wtt01"
