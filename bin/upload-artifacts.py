a1f6367ac05b4f34542d9553c6e5ed099451a9d8
Trent Hauck <trent@trenthauck.com>
2023-03-06 14:44:47 -0800

    bump: version 0.1.26.dev3 → 0.1.26.dev4

--- bin/upload-artifacts.py
+++ bin/upload-artifacts.py
@@ -13,7 +13,7 @@ lambda_client = boto3.client("lambda")
 
 if __name__ == "__main__":
     name = "wtt01"
-    version = "0.1.26.dev3"
+    version = "0.1.26.dev4"
     handler = "WTTArtifactHandler"
 
     environment = os.environ["ENVIRONMENT"]
