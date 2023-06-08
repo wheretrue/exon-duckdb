946f8e55889498f57901c4f573f805b304ae358c
Trent Hauck <trent@trenthauck.com>
2023-06-07 19:16:01 -0700

    feat: more cleanup

--- wtt01_rust/build.rs
+++ wtt01_rust/build.rs
@@ -4,10 +4,8 @@ use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    // let out_dir = env::var_os("OUT_DIR").unwrap();
     let out_dir = "./../src/include/";
 
-    // Join the out_dir with the name of the file we want to generate.wtt01_rust
     let dest_path = std::path::Path::new(&out_dir).join("wtt01_rust.hpp");
 
     cbindgen::Builder::new()
