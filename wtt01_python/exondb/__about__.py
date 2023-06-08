4486fa435e70a4484706519ac09892427889f72a
Trent Hauck <trent@trenthauck.com>
2023-06-07 18:55:52 -0700

    feat: streamline

--- wtt01_python/exondb/__about__.py
+++
@@ -1,5 +0,0 @@
-"""About exondb."""
-
-__version__ = "v0.3.9"
-
-EXTENSION_NAME = "exondb"
--- wtt01_python/exondb/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Package for exondb."""
-
-from exondb.__about__ import __version__
-from exondb.main import get_connection, run_query_file
-
-__all__ = ["get_connection", "run_query_file", "__version__"]
